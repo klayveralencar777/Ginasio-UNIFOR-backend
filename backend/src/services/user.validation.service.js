98a289f07e4164d2ea3c667ff16c1d435a54ca61
Shariza <166448916+Shizaricchi@users.noreply.github.com>
2026-05-29 14:45:16 -0300

    Modify user validation to exclude user ID in checks
    Update user validation methods to exclude specific user IDs when checking for existing emails and CPFs.

--- backend/src/services/user.validation.service.js
+++ backend/src/services/user.validation.service.js
@@ -4,18 +4,14 @@ import { UserRepository } from "../repositories/user.repository.js";
 export class UserValidationService {
     constructor() { this.userRepository = new UserRepository()}
 
-    async checkUserByEmail(email) {
-        const user = await this.userRepository.findByEmail(email);
-        if(user) {
-            throw new EmailAlreadyExists(`Já existe um usuário com o email ${email}`);
-        }
-    }
-    async checkUserByCpf(cpf) {
-        const user = await this.userRepository.findByCpf(cpf);
-        if(user) {
-            throw new CpfAlreadyExists(`Já existe um usuário com o cpf ${cpf}`);
-        }
-    }
+    async checkUserByEmail(email, excludeId = null) {
+    const user = await this.userRepository.findByEmail(email);
+    if (user && user.id !== excludeId) throw new EmailAlreadyExists();
+}
+    async checkUserByCpf(cpf, excludeId = null) {
+    const user = await this.userRepository.findByCpf(cpf);
+    if (user && user.id !== excludeId) throw new CpfAlreadyExists();
+}
 
     async checkIsFuncionario(userId) {
          const user = await this.userRepository.findById(userId);
